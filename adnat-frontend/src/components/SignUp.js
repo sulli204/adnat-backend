d3dfec8775c3057156c128efdc51e10eee66727e
Patrick <infiniteboners@gmail.com>
2021-08-13 12:22:14 -0500

    Added password requirements in label

--- adnat-frontend/src/components/SignUp.js
+++ adnat-frontend/src/components/SignUp.js
@@ -82,7 +82,7 @@ const SignUp = () => {
                     </div>
                     <div class="row">
                         <input type="password" name="password" id="password" value={password} onChange={(e) => { setPassword(e.target.value) }} />
-                        <label for="password">Password</label>
+                        <label for="password">Password (at least 6 characters)</label>
                     </div>
                     <div class="row">
                         <div class="red-text">{error}</div>
